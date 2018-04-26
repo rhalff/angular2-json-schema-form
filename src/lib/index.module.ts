fc47cb0655633c359756d2cb028c43e374e03bea
Rob Halff <rob.halff@gmail.com>
2018-04-26 16:26:12 +0100

    point to index

---
+++ src/lib/index.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+
+// Importing anything here from @angular/material will throw the Illegal State
+import { MatButton } from '@angular/material';
+
+@NgModule({})
+export class MaterialDesignFrameworkModule {}
