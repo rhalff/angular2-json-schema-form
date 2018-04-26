b1253849a324e77fb36efe1d536713a97b41d87b
Rob Halff <rob.halff@gmail.com>
2018-04-26 16:29:04 +0100

    upgrade all @angular packages

---
+++ upgrade.sh
@@ -0,0 +1,25 @@
+npm i \
+@angular/cdk@next \
+@angular/common@next \
+@angular/core@next \
+@angular/flex-layout@next \
+@angular/forms@next \
+@angular/material@next \
+@angular/platform-browser@next \
+@angular-devkit/build-angular \
+@angular-devkit/build-ng-packagr \
+@angular/animations@next \
+@angular/cdk@next \
+@angular/cli@next \
+@angular/common@next \
+@angular/compiler@next \
+@angular/compiler-cli@next \
+@angular/core@next \
+@angular/flex-layout@next \
+@angular/forms@next \
+@angular/http@next \
+@angular/language-service@next \
+@angular/material@next \
+@angular/platform-browser@next \
+@angular/platform-browser-dynamic@next \
+@angular/router@next
