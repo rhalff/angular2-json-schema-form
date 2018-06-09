2f2f043ac3555982ed893fcfef4d7d723ae963b1
Rob Halff <rob.halff@gmail.com>
2018-06-09 03:24:01 +0200

    Updates

--- angular2-json-schema-form.d.ts
+++ angular2-json-schema-form.d.ts
@@ -2,8 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { MATERIAL_FRAMEWORK_COMPONENTS as ɵe } from './src/framework-library/material-design-framework/index';
-export { ANGULAR_MATERIAL_MODULES as ɵc } from './src/framework-library/material-design-framework/material-design-framework.module';
+export { MATERIAL_FRAMEWORK_COMPONENTS as ɵd } from './src/framework-library/material-design-framework/index';
+export { ANGULAR_MATERIAL_MODULES as ɵb } from './src/framework-library/material-design-framework/material-design-framework.module';
 export { JSON_SCHEMA_FORM_VALUE_ACCESSOR as ɵa } from './src/json-schema-form.component';
-export { loadFramework as ɵb } from './src/json-schema-form.module';
-export { BASIC_WIDGETS as ɵd } from './src/widget-library/index';
+export { BASIC_WIDGETS as ɵc } from './src/widget-library/index';
--- src/json-schema-form.module.d.ts
+++ src/json-schema-form.module.d.ts
@@ -1,5 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
 export declare function loadFramework(framework: any): any;
 export declare class JsonSchemaFormModule {
-    static forRoot(framework: any): ModuleWithProviders;
+    static forRoot(FormFramework: any): ModuleWithProviders;
 }
