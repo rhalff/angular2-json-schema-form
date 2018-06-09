c673c8537407bee702bf1d90c98bf073f1862ebd
Rob Halff <rob.halff@gmail.com>
2018-06-09 03:40:08 +0200

    Updates

--- src/json-schema-form.module.d.ts
+++ src/json-schema-form.module.d.ts
@@ -1,5 +1,4 @@
 import { ModuleWithProviders } from '@angular/core';
-export declare function loadFramework(framework: any): any;
 export declare class JsonSchemaFormModule {
     static forRoot(FormFramework: any): ModuleWithProviders;
 }
