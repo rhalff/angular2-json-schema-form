49af842b6ef9c288cd7aa71bc10b68c848e46273
Rob Halff <rob.halff@gmail.com>
2020-02-14 23:41:12 +0100

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+npm run deploy:bootstrap3-framework
+npm run deploy:bootstrap4-framework
+npm run deploy:common
+npm run deploy:json-schema-form
+npm run deploy:material-design-framework
+npm run deploy:no-framework
+npm run deploy:widget-library
