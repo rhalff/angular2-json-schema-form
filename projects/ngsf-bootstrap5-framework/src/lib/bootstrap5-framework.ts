726107e93ab8a6490b9451585482f5cc581174a1
Robbert Halff <rob.halff@gmail.com>
2024-08-03 20:32:05 +0200

    Re-add popper.

--- projects/ngsf-bootstrap5-framework/src/lib/bootstrap5-framework.ts
+++ projects/ngsf-bootstrap5-framework/src/lib/bootstrap5-framework.ts
@@ -16,6 +16,7 @@ export class Bootstrap5Framework extends Framework {
 
 
   scripts = [
+    '//cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
     '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
   ]
 }
